4cd7a8df9eac4301dd5711f566cfe445898fadb2
Louiexz <fluizlucas@gmail.com>
2024-06-06 18:22:51 -0300

    Ajustando diretorio

---
+++ telaLogin/js/link.js
@@ -0,0 +1,9 @@
+import { LinkFuncoes } from "./linkFuncoes.js"
+
+const funcao = new LinkFuncoes();
+
+// Configurar eventos comuns em todas as telas
+funcao.configurarEventosComuns();
+
+// Determinar a tela atual e configurar eventos específicos
+funcao.configurarEventosLogin();
