77d0c43bc31d6576635ec2fff2321b3d393e65e8
Louiexz <fluizlucas@gmail.com>
2024-06-08 16:11:26 -0300

    Responsividade e script atualizado

---
+++ telaCarrinho/js/script.js
@@ -0,0 +1,64 @@
+atualizaValor();
+
+var diminuir = document.querySelector("#diminuir");
+diminuir.addEventListener('click', function() {
+  var valorElement = document.querySelector('#valor');
+  var valor = parseInt(valorElement.textContent);
+
+  if (valor > 1) {
+    valorElement.textContent = String(valor - 1);
+    atualizaValor();
+  }
+});
+
+var aumentar = document.querySelector("#aumentar");
+aumentar.addEventListener('click', function() {
+  var valorElement = document.querySelector('#valor');
+  var valor = parseInt(valorElement.textContent);
+
+  valorElement.textContent = String((valor + 1));
+  atualizaValor();
+});
+
+function atualizaValorTotal(valorProduto) {
+  var valorTotal = document.querySelectorAll(".total-valor");
+  var valorFrete = parseFloat(document.querySelector("#resumo-frete").textContent);
+
+  valorTotal.forEach(element => {
+    if (element.id == "pix") {
+      element.textContent = String(parseInt(valorProduto * 0.9) + valorFrete);
+    } else {
+      element.textContent = String(parseInt(valorProduto) + valorFrete);
+
+      atualizaValorCartao(element.textContent);
+    }
+  });
+}
+function atualizaValorCartao(valor) {
+  document.querySelector("#parcelas-cartao").textContent = parseInt(valor / 3);
+}
+function atualizaValor() {
+  var quantidade = parseInt(document.querySelector('#valor').textContent);
+
+  var valorProduto = parseFloat(document.querySelector("#produto-valor").textContent);
+
+  var valorResumo = document.querySelector("#resumo-valor");
+  valorResumo.textContent = String(valorProduto * quantidade);
+
+  atualizaValorTotal(valorResumo.textContent);
+}
+
+document.querySelectorAll(".input-frete").forEach(element => {
+  element.addEventListener('click', () => {
+    console.log(element);
+    if (element.checked) {
+      document.querySelector("#resumo-frete").textContent = String(element.value);
+
+      atualizaValor();
+      }
+    });
+});
+
+document.querySelector("#continuar").addEventListener('click', function() {
+  window.location.href = "../telaPagamento/pagamento.html";
+})
