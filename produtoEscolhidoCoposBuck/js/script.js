6e7cf801d2dae012cfcc88a75da8c59eaf274f74
Artur <arturramos093@gmail.com>
2024-06-14 00:04:24 -0300

    adicionando interação no icon de carrinho

--- produtoEscolhidoCoposBuck/js/script.js
+++ produtoEscolhidoCoposBuck/js/script.js
@@ -75,3 +75,22 @@ document.addEventListener('DOMContentLoaded', function() {
     botaoMenos.addEventListener("click", subtrair);
 });
 
+//JS do icon carrinho
+
+document.addEventListener('DOMContentLoaded', function() {
+    const carrinhoIcons = document.querySelectorAll('.carrinhoIcon i');
+  
+    carrinhoIcons.forEach(icon => {
+      icon.addEventListener('click', function(event) {
+        event.preventDefault();
+  
+        if (icon.classList.contains('fa-cart-plus')) {
+          icon.classList.remove('fa-cart-plus');
+          icon.classList.add('fa-cart-arrow-down');
+        } else {
+          icon.classList.remove('fa-cart-arrow-down');
+          icon.classList.add('fa-cart-plus');
+        }
+      });
+    });
+  });
